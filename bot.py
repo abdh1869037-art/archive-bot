c14ca8cdf2fb2392f70e5eeb55f7326ae5c91451
abdh1869037-art <abdh1869037-art@users.noreply.github.com>
2026-08-14 20:29:56 +0000

    Remove hardcoded bot token - use only GitHub Secrets

--- bot.py
+++ bot.py
@@ -7,7 +7,9 @@ from datetime import datetime
 from template_handler import start_template_session, handle_template_photo, template_sessions
 
 # --- الإعدادات الأساسية ---
-API_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '8951535425:AAHUWdQgR36yjvIq-6NdUt1sIDrreYXGAuE')
+API_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+if not API_TOKEN:
+    raise ValueError('TELEGRAM_BOT_TOKEN environment variable is not set')
 bot = telebot.TeleBot(API_TOKEN)
 
 # --- ملف حفظ الرموز ---
